66561d44e9a306973500ba812da57fa4eb563b69
Ryan <hello@ryanwu.me>
2019-01-22 17:14:01 -0500

    basic

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,30 +9,7 @@ module.exports = {
       useFsEvents: false,
     },
     env: {
-      DEBUG: 'Client*',//DB*
       NODE_ENV: 'development',
-      BLUEBIRD_WARNINGS: 0,
     },
-  }, {
-    name: 'pk-int',
-    node_args: '--max-old-space-size=2048',
-    script: 'dist/index.js',
-    env: {
-      NODE_ENV: 'integration',
-    },
-  }, {
-    name: 'pk-prod',
-    node_args: '--max-old-space-size=2048',
-    source_map_support: false,
-    max_memory_restart: '4G',
-    script: 'dist/index.js',
-    vizion: false,
-    env: {
-      NODE_ENV: 'production',
-      BLUEBIRD_WARNINGS: 0,
-      PORT: 3000,
-    },
-    exec_mode: 'cluster',
-    instances: 'max',
   }],
 }
--- server/app.js
+++ server/app.js
@@ -1,39 +1,21 @@
+import bodyParser from 'body-parser'
+import errorhandler from 'errorhandler'
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+import session from 'express-session'
+
 import '../config' //Enable global.AppConfig
 import Logger from './lib/Logger'
 import Response from './lib/Response'
-import errorhandler from 'errorhandler'
-import express from 'express'
-import bodyParser from 'body-parser'
-import morgan from 'morgan'
 
 global.Logger = Logger(AppConfig.LOG_LEVEL)
 global.Response = Response
 
 const app = express()
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(AppConfig.isDevelopment ?
-    morgan('dev') :
-    morgan('combined', {
-        skip: function (req, res) {
-            return res.statusCode < 400
-        }
-    }))
-app.use(bodyParser.json({
-    limit: AppConfig.HTTP_BODY_LIMIT
-}))
-
-app.get('/', (req, res) => {
-    res.json({status: 200})
-})
-app.get('/error', (req, res, next) => {
-    next(new Error('te'))
-})
-
-app.use(() => {
-    const error = new Error('Resource Not Found')
-    error.status = 404
-    throw error
-})
 if (AppConfig.isDevelopment) {
     const notifier = require('node-notifier');
     app.use(errorhandler((err, str, req) => {
@@ -44,6 +26,39 @@ if (AppConfig.isDevelopment) {
         })
     }))
 }
+app.use(AppConfig.isDevelopment ?
+    morgan('dev') :
+    morgan('combined', {
+        skip: function (req, res) {
+            return res.statusCode < 400
+        }
+    }))
+app.use(bodyParser.json({
+    limit: AppConfig.HTTP_BODY_LIMIT
+}))
+app.use(session({
+    secret: 'SESSION_KEY',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true
+    }
+}))
+app.get('/', (req, res) => {
+    res.json({
+        status: 200
+    })
+})
+app.get('/error', (req, res, next) => {
+    next(new Error('te'))
+})
+
+app.use(() => {
+    const error = new Error('Resource Not Found')
+    error.status = 404
+    throw error
+})
+
 app.use(require('./routes/error'))
 
 module.exports = app
